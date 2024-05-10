cc22cf517fc56727d02d4fb2d91fd6a953d248d9
Tu Trinh <tutrinh@rnn.ist.berkeley.edu>
2024-05-09 20:10:32 -0700

    sixth? attempt

--- src/training/train.py
+++ src/training/train.py
@@ -972,7 +972,10 @@ def process_attention_scores(scores, tokens, og_img, og_idx, args, average_over_
                 token_attn = layer_attn[i]
                 min_token_attn = token_attn.min()
                 max_token_attn = token_attn.max()
-                token_attn = (token_attn - min_token_attn) / (max_token_attn - min_token_attn) if max_token_attn > min_token_attn else np.zeros_like(token_attn)
+                if max_token_attn > min_token_attn:
+                    token_attn = 0.2 + 0.8 * (token_attn - min_token_attn) / (max_token_attn - min_token_attn)
+                else:
+                    token_attn = np.zeros_like(token_attn)
                 for j in range(scores.size(-1)):  # for each of 255 patches
                     row = j // num_patches_side
                     col = j % num_patches_side
